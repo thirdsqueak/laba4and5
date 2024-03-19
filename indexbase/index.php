1afbf48991fa49039d7eb62fc371ea3d45e34a50
Lesha <lvasilkov952@gmail.com>
2024-03-19 12:41:43 +0000

    update to laba 5 index.php

---
+++ indexbase/index.php
@@ -0,0 +1,75 @@
+<!-- <?php
+// Загрузка данных из файла CSV
+$data = array_map('str_getcsv', file('titanic.csv'));
+$columns = array_shift($data);
+$passengers = array();
+foreach ($data as $row) {
+    $passengers[] = array_combine($columns, $row);
+}
+
+// Поиск пассажиров по возрасту
+function searchByAge($passengers, $age) {
+    $results = array();
+    foreach ($passengers as $passenger) {
+        if ($passenger['Age'] == $age) {
+            $results[] = $passenger;
+        }
+    }
+    return $results;
+}
+
+// Поиск пассажиров по имени с использованием регулярного выражения
+function searchByName($passengers, $name) {
+    $results = array();
+    foreach ($passengers as $passenger) {
+        if (preg_match("/$name/i", $passenger['Name'])) {
+            $results[] = $passenger;
+        }
+    }
+    return $results;
+}
+
+// Получение параметров запроса
+$age = isset($_GET['age']) ? $_GET['age'] : '';
+$name = isset($_GET['name']) ? $_GET['name'] : '';
+
+// Выполнение поиска
+if (!empty($age)) {
+    $results = searchByAge($passengers, $age);
+} elseif (!empty($name)) {
+    $results = searchByName($passengers, $name);
+} else {
+    $results = $passengers;
+}
+
+// Создание HTML-страницы с результатами
+$html = '<html>
+<head>
+    <title>Результаты поиска пассажиров</title>
+</head>
+<body>
+    <h1>Результаты поиска пассажиров</h1>
+    <table>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Пол</th>
+            <th>Класс</th>
+            <th>Выжил</th>
+        </tr>';
+foreach ($results as $result) {
+    $html .= '<tr>
+                <td>'.$result['Name'].'</td>
+                <td>'.$result['Age'].'</td>
+                <td>'.$result['Sex'].'</td>
+                <td>'.$result['Pclass'].'</td>
+                <td>'.$result['Survived'].'</td>
+            </tr>';
+}
+$html .= '</table>
+</body>
+</html>';
+
+// Вывод HTML-страницы
+echo $html;
+?>
